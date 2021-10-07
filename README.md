bd50736d85d05e79cfc5576d22b27d6367a17528
guyagiv <guyagiv@gmail.com>
2021-10-07 11:00:07 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Voyage
+# compass
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'voyage'`, () => {
+  it(`should have as title 'compass'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('voyage');
+    expect(app.title).toEqual('compass');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('voyage app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'compass app is running!'
+    );
   });
 });
