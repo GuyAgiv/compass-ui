383b89197dab1a20a608210649a2f87e001770c8
guyagiv <guyagiv@gmail.com>
2021-10-07 17:52:27 +0300

    map component and service has been added!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoiZ3V5YWdpdiIsImEiOiJja3VnenIzeTYwZWk1MnFteW91NzdiZ2VtIn0.mIUz_vkMXewycYfWCm8yyw',
+  },
 };
 
 /*
